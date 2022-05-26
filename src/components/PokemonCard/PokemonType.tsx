a780d4376353763adfc54bf155482c97896ae330
gui-beraldo <gmullerberaldo@gmail.com>
2022-05-26 09:57:37 -0300

    starting PokemonType component

---
+++ src/components/PokemonCard/PokemonType.tsx
@@ -0,0 +1,7 @@
+export function PokemonType(props: any) {
+	return <div>{props.type}</div>;
+}
+
+// {pokemon.types.map((types: any) => {
+//   return <div key={types.type.name}>{types.type.name}</div>;
+// })}
--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { getPokemon } from 'services/gettingPokemons';
 import { Card } from './Card';
+import { PokemonType } from './PokemonType';
 
 export function PokemonCard({ pokemonUrl }: any) {
 	const [loading, setLoading] = useState<boolean>(true);
 	const [pokemon, setPokemon] = useState<any>();
+	const zeroPad = (num: any, places: any) => String(num).padStart(places, '0');
 
 	useEffect(() => {
 		async function fetchData() {
@@ -23,13 +25,17 @@ export function PokemonCard({ pokemonUrl }: any) {
 				<>
 					<Card pokemonType={pokemon.types[0].type.name}>
 						<div className="z-10 flex flex-col">
-							<p className="font-bold text-xs leading-[1.2]">{pokemon.id}</p>
+							<p className="before:content-['#'] font-bold text-xs leading-[1.2] text-[#17171b] opacity-60">
+								{zeroPad(pokemon.id, 3)}
+							</p>
 							<p className="font-bold text-[26px] leading-[1.2] capitalize text-white">
 								{pokemon.name}
 							</p>
-							<div>
+							<div className="flex">
 								{pokemon.types.map((types: any) => {
-									return <div key={types.type.name}>{types.type.name}</div>;
+									return (
+										<PokemonType key={types.type.name} type={types.type.name} />
+									);
 								})}
 							</div>
 						</div>
