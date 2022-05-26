4f692712c8fecdbf989713aa29d6623bd3a3bfe7
gui-beraldo <gmullerberaldo@gmail.com>
2022-05-26 11:09:07 -0300

    pokemonType badge margin

--- src/components/PokemonCard/PokemonType.tsx
+++ src/components/PokemonCard/PokemonType.tsx
@@ -7,7 +7,7 @@ export function PokemonType(props: any) {
 		<div
 			className={
 				(type === undefined ? 'bg-zinc-300' : type.badgeClass) +
-				' p-[5.5px_5.5px_5.5px_25px] rounded-[3px] bg-no-repeat bg-[length:15px_15px] bg-[center_left_5px]'
+				' p-[5.5px_5.5px_5.5px_25px] mr-[5px] rounded-[3px] bg-no-repeat bg-[length:15px_15px] bg-[center_left_5px]'
 			}
 		>
 			<p className="text-white font-medium text-xs capitalize">{props.type}</p>
