e2880cd0385acdeda0d4601003f816f8902aab4c
gui-beraldo <gmullerberaldo@gmail.com>
2022-05-26 10:06:17 -0300

    PokemonType card

--- src/components/PokemonCard/PokemonType.tsx
+++ src/components/PokemonCard/PokemonType.tsx
@@ -1,3 +1,16 @@
+import Pokemon from '../../PokemonType.json';
+
 export function PokemonType(props: any) {
-	return <div className="text-white font-medium text-xs">{props.type}</div>;
+	const type = Pokemon[props.type];
+
+	return (
+		<div
+			className={
+				(type === undefined ? 'bg-zinc-300' : type.bgClass) +
+				' p-[5.5px] rounded-[3px]'
+			}
+		>
+			<p className="text-white font-medium text-xs capitalize">{props.type}</p>
+		</div>
+	);
 }
